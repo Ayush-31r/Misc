c9f260310830f2fdc29631e39fd5b4bcf40d61ab
Ayush-31r <rai4ayush@gmail.com>
2025-11-25 00:34:30 +0530

    New Game idea

--- MiniGame.py
+++ MiniGame.py
@@ -9,3 +9,16 @@ FOOD_COUNT = 120
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+
+# ---------- ENTITIES ----------
+
+class Creature:
+    def __init__(self, x, y, dna=None):
+        self.x = x
+        self.y = y
+        self.energy = 100
+
+        self.dna = dna if dna is not None else np.random.rand(3)
+        self.speed = self.dna[0] * 2 + 0.5
+        self.vision = self.dna[1] * 120 + 20
+        self.efficiency = self.dna[2]
